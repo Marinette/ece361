647f10c7fb3d9ddf54ed1d088235d0196dca71a8
Marinette <marinette.m.chen@gmail.com>
2019-03-03 21:14:17 -0500

    yes

---
+++ Lab3/lab3/lab3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lab3
+//
+//  Created by Marinette Chen on 2019-03-03.
+//  Copyright © 2019 Marinette Chen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
