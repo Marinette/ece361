d8d877a3b6527d02843882260979199885ea0895
Ubuntu <ubuntu@ece361.(none)>
2019-03-27 21:31:25 -0400

    fixed

--- Lab5/tapping_traffic.py
+++ Lab5/tapping_traffic.py
@@ -1,13 +1,11 @@
 # yo yo this is marinette yo
 import ryu_ofctl
 flow = ryu_ofctl.FlowEntry()
-act1 = ryu_ofctl.OutputAction(3)
-act2 = ryu_ofctl.OutputAction(1)
+act1 = ryu_ofctl.OutputAction(2)
 
-flow.in_port = 2
+flow.in_port = 1
 flow.addAction(act1)
-flow.addAction(act2)
 
 dpid = '1'  # '0x1' or '0000000000000001' works as well
 ryu_ofctl.insertFlow(dpid, flow)
-printf('Success!')
+print('Success!')
